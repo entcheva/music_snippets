4321f2bdef497f49e18a25264c363ecc34a97ad5
entcheva <christina.entcheva@gmail.com>
2017-03-10 21:08:43 -0500

    Add users table

--- music-snippets-API/app/controllers/snippets_controller.rb
+++ music-snippets-API/app/controllers/snippets_controller.rb
@@ -1,3 +1,33 @@
 class UsersController < ApplicationController
 
+  def index
+    @users = User.all
+  end
+
+  def new
+    @user = User.new(user_params)
+    @user.save
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def delete
+  end
+
+  def destroy
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :email, :password, :password_digest)
+  end
+
 end
--- music-snippets-API/app/controllers/users_controller.rb
+++ music-snippets-API/app/controllers/users_controller.rb
@@ -1,3 +1,24 @@
 class UsersController < ApplicationController
 
+  def index
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def delete
+  end
+
+  def destroy
+  end
+
 end
