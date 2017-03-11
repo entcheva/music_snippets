58fdeb3473984ce2a0b609b4dc6f37a2cb698f54
entcheva <christina.entcheva@gmail.com>
2017-03-11 15:20:46 -0500

    Add basic User CRUD

---
+++ music_snippets_API/app/controllers/api/v1/users_controller.rb
@@ -0,0 +1,59 @@
+module Api::V1
+
+  class UsersController < ApplicationController
+
+    def index
+      @users = User.all
+      render json: @users
+    end
+
+    def new
+      find_user
+      render json: @user
+    end
+
+    def create
+      @user = User.create(user_params)
+
+      # if params[:password] === params[:password_confirmation]
+      #   @user.password = params[:password]
+      #   @user.save
+      #   render json: {jwt: jwt}
+      # else
+      #   render json: {:errors=> [{:detail=>"failed password verification", :source=>{:pointer=>"user/err_type"}} ] }, status: 404
+      # end
+
+    end
+
+    def edit
+      find_user
+    end
+
+    def update
+      find_user
+      @user = User.update(user_params)
+    end
+
+    def delete
+      find_user
+    end
+
+    def destroy
+      @user.destroy
+      @users = User.all
+      render json: @users
+    end
+
+    private
+
+    def user_params
+      params.require(:user).permit(:username, :email, :password, :password_confirmation)
+    end
+
+    def find_user
+      @user = User.find(params[:id])
+    end
+
+  end
+
+end
--- music_snippets_API/config/routes.rb
+++ music_snippets_API/config/routes.rb
@@ -1,8 +1,14 @@
 Rails.application.routes.draw do
 
-  scope module: 'api' do
+  namespace 'api' do
     namespace :v1 do
       resources :users, :snippets
+      # get '/users', to: 'users#index', as: 'users'
+      # get '/users/new', to: 'users#new'
+      # get '/users/:id', to: 'users#show'
+      # get '/users/:id/edit', to: 'users#edit'
+      # patch '/users/:id/edit', to: 'users#edit'
+
     end
   end
 
