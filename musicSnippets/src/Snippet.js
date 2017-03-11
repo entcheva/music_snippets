47d6f22d0981135d0164a3437bf21fca5cd0dca8
entcheva <christina.entcheva@gmail.com>
2017-03-11 17:57:37 -0500

    Render User and Snippet components

---
+++ musicSnippets/src/Snippet.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+export default class Snippet extends Component {
+  render() {
+    return (
+      <div>Hiiii from the Snippet component</div>
+    );
+  }
+}
