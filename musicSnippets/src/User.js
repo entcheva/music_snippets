d722c7f87545f3d1e163e9639f5947f7472beaa5
entcheva <christina.entcheva@gmail.com>
2017-03-11 19:56:34 -0500

    Add redux, react-redux

--- musicSnippets/src/User.js
+++ musicSnippets/src/User.js
@@ -1,9 +1,68 @@
 import React, { Component } from 'react';
 
 export default class User extends Component {
+
+  constructor() {
+    super();
+    this.handleSignUp = this.handleSignUp.bind(this);
+    this.handleLogIn = this.handleLogIn.bind(this);
+  }
+
+  handleSignUp() {
+    event.preventDefault();
+    let username = this.refs.username.value;
+    let email = this.refs.email.value;
+    let password = this.refs.password.value;
+    let passwordConfirmation = this.refs.passwordConfirmation.value;
+
+    // if (username !== "" && email !== "" && password !== "" && passwordConfirmation !== "") {
+    //   let user = { username: username, email: email, password: password, password_confirmation: passwordConfirmation };
+    //   this.props.createUser(user);
+    // }
+  }
+
+  handleLogIn() {
+    event.preventDefault();
+  }
+
   render() {
     return (
-      <div>Hiiii from the User component</div>
+      <div>
+
+        <div id="signUp">
+          <h2>Sign Up</h2>
+          <form onSubmit={ this.handleSignUp } >
+            <label>Username: </label>
+            <input ref="username" placeholder="Enter Username" required />
+            <br/><br/>
+            <label>Email: </label>
+            <input type="email" ref="email" placeholder="Enter Email" required />
+            <br/><br/>
+            <label>Password: </label>
+            <input type="password" ref="password" placeholder="Enter Password" required />
+            <br/><br/>
+            <label>Confirm Password: </label>
+            <input type="password" ref="passwordConfirmation" placeholder="Confirm Password" required />
+            <br/><br/>
+            <button type="submit">Sign Up</button>
+          </form>
+        </div>
+
+        <div id="logIn">
+          <h2>Log In</h2>
+          <form onSubmit={ this.handleLogIn }>
+            <label>Email: </label>
+            <input ref="logInEmail" placeholder="Enter Email" />
+            <br/><br/>
+            <label>Password: </label>
+            <input type="password" ref="logInPassword" placeholder="Enter Password"/>
+            <br/><br/>
+            <button type="submit">Log In</button>
+          </form>
+        </div>
+
+
+      </div>
     );
   }
 }
--- musicSnippets/src/index.js
+++ musicSnippets/src/index.js
@@ -1,9 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+
+
 import App from './App';
 import './index.css';
 
+const store = createStore(rootReducer);
+
 ReactDOM.render(
+  <Provider store={store}>
   <App />,
   document.getElementById('root')
+  </Provider>
 );
